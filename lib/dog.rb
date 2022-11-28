d5825c3de5e4bcef0bda410d779bb58fe17d7cd7
Erik Russell <erikrussells@gmail.com>
2022-11-28 13:17:13 -0500

    commit tree ca7e126cd2ed369b01190a74fb62e66cb7031413

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
 
+    def name=(sid)
+        @name = sid
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(bulldog)
+        @breed = bulldog
+    end
+
+    def breed
+        @breed
+    end
 end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
 
+    def name=(rufus)
+        @name = rufus
+    end
+
+    def name
+        @name
+    end
+
+    def job=(trash)
+        @job = trash
+    end
+
+    def job
+        @job
+    end
 end
